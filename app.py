418340f9083b22fe52c2551ab08c63efc6ea3885
Lars Kellogg-Stedman <lars@oddbit.com>
2024-02-06 11:01:37 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from kubernetes import client, config
+
+def create_app():
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    app = FastAPI()
+
+    @app.get('/pods/{namespace}')
+    def list_pods(namespace: str):
+        pods = v1.list_namespaced_pod(namespace)
+        return [pod.metadata.name for pod in pods.items]
+
+    return app
+
+app = create_app()
